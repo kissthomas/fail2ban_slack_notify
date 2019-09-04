cb56daebfa95453362ecb14a9e334225a5f7ec6a
Thomas Kiss <mail@kissthomas.hu>
2019-09-04 19:53:57 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fail2ban_slack_notify
 Slack notifier for fail2ban
+
+You can simply copy the content of this repo to your fail2ban directory.
+
+The provided jail.local has an action which extends the default action_mwl with 
+the slack notification.
+
+You need to change some config variables in slack_notify.sh to start using it.
---
+++ slack_notify.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# *** Configuration ***
+
+# Your personal Slack URL key
+SLACK_HOOK=TKE4P7ERY/BMDCQU9T5/FdzfsMunYIzcOjSfGvYZAF54
+# The channel name to post
+CHANNEL="#monitoring"
+# Username for posting
+USERNAME="fail2ban"
+# Icon used for title
+ICON=":cop:"
+# Default color for the post
+COLOR="#A9A9A9"
+# Banned string to match in message (changes color to danger)
+BANNED="Banned"
+# Unbanned string to match in message (changes color to good)
+UNBANNED="Unbanned"
+
+
+# *** DO NOT EDIT BELOW THIS LINE, unless you know what you're doing :) ***
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+MESSAGE=$1
+HOOK_URL=https://hooks.slack.com/services/${SLACK_HOOK}
+HOST=$(hostname -f)
+
+if [ $# -eq 2 ]; then
+    IP=$2
+    IPHOST=$(dig +short -x $IP)
+    if [ -z "$IPHOST" ]; then
+        IPHOST="n/a"
+    fi
+    COUNTRY=$(curl -s ipinfo.io/${IP}/country)
+    COUNTRY=$(echo "$COUNTRY" | tr -s  '[:upper:]'  '[:lower:]')
+    COUNTRY=":flag-$COUNTRY:"
+else
+    COUNTRY=":flags:"
+fi
+
+MESSAGE="${MESSAGE/_country_/$COUNTRY}"
+
+if [[ "$MESSAGE" =~ "$BANNED" ]]; then
+    COLOR="danger"
+elif [[ "$MESSAGE" =~ "$UNBANNED" ]]; then
+    COLOR="good"
+fi
+
+PAYLOAD="{
+    \"channel\": \"${CHANNEL}\",
+    \"username\": \"${USERNAME}\",
+    \"attachments\": [
+        {
+            \"color\": \"${COLOR}\",
+            \"fallback\": \"${MESSAGE}\",
+            \"fields\": [
+                {
+                    \"short\": true,
+                    \"title\": \"IP Address\",
+                    \"value\": \"${IP}\"
+                },
+                {
+                    \"short\": true,
+                    \"title\": \"Host name\",
+                    \"value\": \"${IPHOST}\"
+                }
+            ],
+            \"footer\": \"Fail2ban Alerts\",
+            \"text\": \"${MESSAGE}\",
+            \"title\": \"${ICON} Fail2ban on ${HOST}\",
+            \"ts\": $(date +%s)
+        }
+    ]
+}"
+
+curl -s -X POST --data-urlencode "payload=$PAYLOAD" ${HOOK_URL}
+
+exit $?
